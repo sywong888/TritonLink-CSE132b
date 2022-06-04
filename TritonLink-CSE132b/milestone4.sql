51e4fe8192314dec48ddf4515846dc2f1ee24155
Sydney Wong <syd.wong888@gmail.com>
2022-06-04 00:20:29 -0700

    add trigger 3

--- TritonLink-CSE132b/milestone4.sql
+++ TritonLink-CSE132b/milestone4.sql
@@ -1,6 +1,7 @@
 /** Trigger 1 **/
 CREATE TRIGGER MEETING_CONFLICT
 BEFORE INSERT ON MEETING
+FOR EACH ROW
 EXECUTE PROCEDURE add_meeting();
 
 CREATE OR REPLACE FUNCTION add_meeting()
@@ -28,28 +29,26 @@ CREATE OR REPLACE FUNCTION add_meeting()
 
 insert into meeting values(14, 'MATH132B-1', 'SP', 2022, 'A00', 'T', '15:00', '16:00', 'room15', 'discussion', 'n');
 
-/** Trigger 2 **/ 
+/** Trigger 3 **/ 
 CREATE TRIGGER PROF_CONFLICT
 BEFORE INSERT ON MEETING
-EXECUTE PROCEDURE add_prof_meeting(new.course_id, new.class_title, new.quarter, new.year, new.section_id);
+FOR EACH ROW
+EXECUTE PROCEDURE add_prof_meeting();
 
 CREATE OR REPLACE FUNCTION add_prof_meeting()
   RETURNS TRIGGER 
   LANGUAGE PLPGSQL
   AS
   $$
-  DECLARE prof INT := (SELECT s.faculty_id FROM section s WHERE s.course_id = new.course_id AND s.class_title = new.class_title AND s.quarter = new.quarter AND s.year = new.year); 
-  
+  declare prof integer;
   BEGIN
-	EXECUTE 'CREATE OR REPLACE VIEW prof_meetings AS
-	SELECT m.*
-	FROM section s, meeting m
-	WHERE s.faculty_id = prof AND s.course_id = ' || $1 || 'AND s.class_title = ' || $2 || 'AND s.quarter = ' || $3 || 'AND s.year = ' || $4 || 'AND s.section_id = ' || $5 || ';';
+	SELECT s.instructor_id into prof FROM section s WHERE s.course_id = new.course_id AND s.class_title = new.class_title AND s.section_id = new.section_id AND s.quarter = new.quarter AND s.year = new.year; 
+	execute format('CREATE OR REPLACE VIEW prof_meetings AS SELECT m.* FROM section s, meeting m WHERE s.instructor_id = %L AND s.course_id = m.course_id AND s.class_title = m.class_title AND s.quarter = m.quarter AND s.year = m.year AND s.section_id = m.section_id;', prof);
 	
 	IF (NOT EXISTS(
       SELECT *
-      FROM section s, meeting m
-      WHERE m.quarter = new.quarter AND m.year = new.year AND m.faculty_id = new.faculty_id AND m.day = new.day AND
+      FROM prof_meetings m
+      WHERE m.quarter = new.quarter AND m.year = new.year AND m.day = new.day AND
             ((new.start_time > m.start_time AND new.start_time < m.end_time) OR
             (new.end_time > m.start_time AND new.end_time < m.end_time) OR
             (new.start_time <= m.start_time AND new.end_time >= m.end_time) OR
@@ -62,7 +61,7 @@ CREATE OR REPLACE FUNCTION add_prof_meeting()
   END;
   $$;
   
-insert into meeting values(2, 'CSE150A-1', 'SP', 2022, 'A00', 'T', '11:00', '12:00', 'room15', 'discussion', 'n');
+insert into meeting values(2, 'CSE150A-1', 'SP', 2022, 'A00', 'R', '21:10', '21:50', 'room15', 'discussion', 'n');
 
 
 
